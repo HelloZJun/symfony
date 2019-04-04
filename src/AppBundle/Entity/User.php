74ee8fb33d8f56927cf926c83220fbfc64f75f5c
ZJun <295741608@qq.com>
2019-04-04 19:22:16 +0800

    Update User.php

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -4,7 +4,9 @@
 namespace AppBundle\Entity;
 
 use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping\OneToMany;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity
